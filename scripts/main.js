3081ceabd77738539648832390040db4a80b04f7
Jimmy <jpham197@gmail.com>
2020-04-27 03:16:25 -0400

    Added functions to handle adding new work experiences

--- scripts/main.js
+++ scripts/main.js
@@ -18,3 +18,44 @@ for (i = 0; i < coll.length; i++) {
         }
     });
 }
+
+let workExperience = [
+    {"title": "IT Intern", "org": "Delta Air Lines", "date": "May 2019 - Aug 2019"},
+    {"title": "TechX Intern", "org": "T-Mobile", "date": "June 2018 - May 2019"},
+    {"title": "Workshop Trainer", "org": "GSU CETL", "date": "June 2017 - May 2018"}
+]
+
+function loadWorkExperience() {
+    let content = document.getElementsByClassName("content")[0];
+    console.log(content);
+
+    workExperience.forEach(experience => {
+        let title = experience.title;
+        let org = experience.org;
+        let date = experience.date;
+        let section = generateWorkExperience(title, org, date);
+        content.appendChild(section);
+    })
+}
+
+function generateWorkExperience(title, organization, frame) {
+    let section = document.createElement("section");
+    let workTitle = document.createElement("span");
+    let org = document.createElement("span");
+    let date = document.createElement("span");
+
+    workTitle.classList.add("work-title");
+    org.classList.add("org");
+    date.classList.add("date");
+
+    workTitle.innerText = title;
+    workTitle.innerText += " ";
+    org.innerText = organization;
+    date.innerText = frame;
+
+    section.appendChild(workTitle);
+    section.appendChild(org);
+    section.appendChild(date);
+
+    return section;
+}
